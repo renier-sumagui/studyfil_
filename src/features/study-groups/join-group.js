88e8f090884afe344be3ac49a1b27c84f964730f
Renier Sumagui <reniersumagui178@gmail.com>
2023-07-12 22:06:29 +0800

    fixed join button

--- src/features/study-groups/join-group.js
+++ src/features/study-groups/join-group.js
@@ -1,7 +1,7 @@
 import Axios from 'axios';
 
 async function joinGroup(userId, groupId) {
-    const response = await Axios.post(`http://localhost:8000/groups/join`, { userId, groupId }, { withCredentials: true });
+    const response = await Axios.post(`https://studyfil-api.onrender.com/groups/join`, { userId, groupId }, { withCredentials: true });
     return response.data;
 }
 
