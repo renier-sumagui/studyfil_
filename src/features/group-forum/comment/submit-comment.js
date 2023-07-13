b9ed3ed82144ff33c89c8032642898e8c74c36fd
Renier Sumagui <reniersumagui178@gmail.com>
2023-07-13 20:04:49 +0800

    dfghlkjxcoviu

--- src/features/group-forum/comment/submit-comment.js
+++ src/features/group-forum/comment/submit-comment.js
@@ -2,6 +2,7 @@ import Axios from 'axios';
 
 async function submitComment(userId, postId, comment, groupId) {
     const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+    console.log('timezone', timezone);
     const response = await Axios.post('https://studyfil-api.onrender.com/comments/create', { userId, postId, comment, groupId, timezone }, { withCredentials: true });
     return response.data;
 }
