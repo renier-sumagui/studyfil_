cb547189749285bbedbff47dc3407ffbfb618163
Renier Sumagui <reniersumagui178@gmail.com>
2023-07-15 20:17:51 +0800

    sdflkj

--- src/features/feedback/feedback.jsx
+++ src/features/feedback/feedback.jsx
@@ -7,7 +7,7 @@ export function Feedback() {
         <>
             <h1 className={FeedbackCss.feedbackHeader}><FeedbackIcon sx={{ fontSize: '1.9rem' }} /> Feedback</h1>
             <h3>Share your comments/suggestions/opinions/reports to help us improve StudyFil</h3>
-            <form className={FeedbackCss.feedbackForm}>
+            <form className={FeedbackCss.feedbackForm} onSubmit={(e) => e.preventDefault()}>
                 <textarea placeholder="Type a message"></textarea>
                 <input type="submit" value="submit" />
             </form>
