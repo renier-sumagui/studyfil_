09dae9be94e004d17deb3626de22eb1aabd56f84
Renier Sumagui <reniersumagui178@gmail.com>
2023-07-27 17:07:14 +0800

    sdflkj

--- src/features/group-forum/comment/submit-vote.js
+++ src/features/group-forum/comment/submit-vote.js
@@ -1,7 +1,8 @@
 import Axios from 'axios';
 
 async function submitVote(id, voteType, isComment, userId) {
-    await Axios.post('https://studyfil-api.onrender.com/votes/', { id, voteType, isComment, userId }, { withCredentials: true })
+    const response = await Axios.post('https://studyfil-api.onrender.com/votes/', { id, voteType, isComment, userId }, { withCredentials: true })
+    return response.data.success;
 }
 
 export { submitVote };
